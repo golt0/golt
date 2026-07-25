e8fcb36b6a09ebc07b53297dfd648d4de19bd37c
Krishna <krishnachaudhary2007gt@gmail.com>
2026-07-26 00:26:09 +0530

    feat(chat): implement message handling routes with authentication

---
+++ apps/backend/src/chat.ts
@@ -0,0 +1,52 @@
+import { Router } from "express";
+import { requireAuth } from "./middlewares/auth.middleware";
+import { prisma } from "@repo/db";
+import { runAgent } from "./agent";
+
+const router = Router();
+
+router.post("/:projectId/messages", requireAuth , async (req , res) => {
+    const ownerId = (req as any).ownerId  as string;
+    const projectId = req.params.projectId  as string;
+
+   const project = await prisma.project.findUnique({
+        where : {id : projectId}
+    })
+
+    if(!project || project.ownerId !== ownerId) {
+        return  res.status(400).json({error : "user not found"})
+    }
+
+    const message = req.body.message;
+    if(!message) {
+        return res.status(400).json({error : "message is required"})
+    }
+
+    runAgent(projectId , message)
+
+    return res.status(200).json({status : "ok"})
+    
+})
+
+router.get("/:projectId/messages", requireAuth, async(req , res) => {
+    const ownerId   = (req as any).ownerId as string;
+    const projectId = req.params.projectId  as string;
+
+    const project = await prisma.project.findUnique({
+        where : {id : projectId}
+    })
+
+    if(!project) {
+        return res.status(400).json({error : "project is not found"})
+    }
+
+    const messaage = await prisma.message.findMany({
+        where : {projectId},
+        orderBy : {createdAt : "asc"}
+    })
+
+    return res.status(200).json({messaage})
+
+})
+
+export default router;
